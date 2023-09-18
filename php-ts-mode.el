e3776bb2c432bf0a1ce1fef1790bccf3f3e64d6c
vpxyz <v.pupillo@gmail.com>
2023-09-18 19:36:22 +0200

    fix file header.

--- php-ts-mode.el
+++ php-ts-mode.el
@@ -1,26 +1,24 @@
 ;;; php-ts-mode.el --- tree-sitter support for PHP  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2023 Free Software Foundation, Inc.
+;; Copyright (C) 2022-2023 Vincenzo Pupillo
 
 ;; Author     : Vincenzo Pupillo <v.pupillo@gmail.com>
 ;; Maintainer : Vincenzo Pupillo <v.pupillo@gmail.com>
 ;; Created    : September 2023
 ;; Keywords   : php language tree-sitter
 
-;; This file is part of GNU Emacs.
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
 
-;; GNU Emacs is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
